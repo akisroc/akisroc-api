b2bcca2dfb07ff2725562a813da0495659bae70f
Adrien-H <adrien.h@tuta.io>
2019-06-02 19:18:21 +0200

    Add Vue.js

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+require('../css/app.css')
+
+import Vue from 'vue'
+import Salut from './components/Salut'
+
+const app = new Vue({
+  el: '#app',
+  components: {Salut}
+})
---
+++ assets/js/components/Salut.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Salut</h1>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss">
+  h1 {
+    color: darkblue;
+  }
+</style>
