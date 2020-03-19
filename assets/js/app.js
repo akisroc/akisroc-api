b760f8ea7cae9d1296105509229abbf27b386c7f
Adrien-H <adrien.h@tuta.io>
2020-03-19 12:03:13 +0100

    Refactor Markdown parsing
    Now handling markdown directly from Twig filter instead of
    Vue component.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,9 @@
 require('../css/app.scss')
 
 import Vue from 'vue'
-import VueMarkdown from 'vue-markdown/src/VueMarkdown';
 
 const app = new Vue({
   el: '#app',
   components: {
-    VueMarkdown
   }
 })
