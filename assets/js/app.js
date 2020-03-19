7bc6eea9de51ac1aca8670ee58d51463cbf5d0df
Adrien-H <adrien.h@tuta.io>
2020-03-19 11:22:42 +0100

    Remove useless PostExcerptCard Vue component

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,10 @@ require('../css/app.scss')
 
 import Vue from 'vue'
 import VueMarkdown from 'vue-markdown/src/VueMarkdown';
-import PostExcerptCard from './components/PostExcerptCard'
 
 const app = new Vue({
   el: '#app',
   components: {
-    VueMarkdown, PostExcerptCard
+    VueMarkdown
   }
 })
